40ba4216db4af73e2adaa05793ad6303accdebc7
soulforger <n.zolbooyuro@gmail.com>
2020-05-18 02:05:16 +0800

    change : design update

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
 
     return (
         <footer className="page-footer row">
-            <div className="col l11 m11 offset-m1 offset-l1 row">
+            <div className="col l12 m12 row" style={{marginBottom:'0'}}>
                 <div className="col l4 m4">
                     <img alt="logo" src={logo} />
                 </div>
-                <div className="col l6 m6 offset-l2 offset-m2">
+                <div className="col l6 m6 offset-l1 offset-m1">
                     <h4 className="center">Холбоо барих</h4>
                     <ul>
                         <li><h6>Утас: +976 99135563</h6></li>
@@ -19,7 +19,7 @@ const Footer = () => {
                         <li><h6>Хаяг: 75-32, Юнескогийн гудамж, 6-р хороо, 13-р хороолол, Баянзүрх дүүрэг, Улаанбаатар хот, Монгол Улс, 13373</h6></li>
                     </ul>
                 </div>
-
+                <div className="line col l12 m12"> </div>
             </div>
             <div className="footer-copyright col l12 m12">
                 <h5 className="container center black-text">
