33380efe8f58cffdbc98f752481afc8c9095da5e
Javkhaa00 <liplicker0423@gmail.com>
2020-05-18 00:54:29 +0800

    feat: added new Register web page

---
+++ src/component/register/Register.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Register.scss";
+import Header from "../Header"
+
+const Register = () => {
+	return (
+		<div className="mainDiv row">
+			<Header />
+			<div className="first-text row col l10 m10 offset-l1 offset-m1">
+				<div className="col l6 m6">
+					<h1 className="inspiration">
+						Таны ирээдүйн
+						өөртөө хийх
+						хөрөнгө оруулалт
+                    </h1>
+					<h5>
+						Залуу сурагчид өөрсдийн амжилтын туршлагаас хуваалцах болно.
+                    </h5>
+				</div>
+				<h1 className="intro col l6 m6 center">
+					NOMT
+					ONLINE
+					ACADEMY
+                </h1>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
