bb8ac06d2e0907952abfce685d943f547a5402ba
soulforger <n.zolbooyuro@gmail.com>
2020-05-17 17:17:52 +0800

    new : teachers page

---
+++ src/component/Teachers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import './Teachers.scss'
+
+
+const Teachers = () => {
+
+
+    return (
+        <div className="teachers row">
+            <Header/>
+        </div>
+    )
+}
+
+export default Teachers
