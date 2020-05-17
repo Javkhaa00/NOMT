4d95e5efe996af3d42cc81380e1258b2f7b56c72
soulforger <n.zolbooyuro@gmail.com>
2020-05-17 17:13:33 +0800

    router added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import "./App.css";
 import FrontPage from "./component/FrontPage";
 import Footer from "./component/footer";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+import Teachers from "./component/Teachers";
 
 function App() {
   return (
-    <div>
-      <FrontPage />
+    <Router>
+      <Switch>
+        <Route path="/teachers">
+          <Teachers />
+        </Route>
+        <Route path="/">
+          <FrontPage />
+        </Route>
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
