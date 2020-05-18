a7259b6959c5b34cca25c98c4882bf9f12bcc78a
Javkhaa00 <liplicker0423@gmail.com>
2020-05-18 23:23:07 +0800

    feat: added new things

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,30 +1,19 @@
 import React from "react";
 import "./Register.scss";
 import Header from "../Header/Header"
-import Footer from "../Footer/footer"
 
 const Register = () => {
 	return (
 		<div className="mainDiv row">
 			<Header />
-			<div className="first-text row col l10 m10 offset-l1 offset-m1">
-				<div className="col l6 m6">
-					<h1 className="inspiration">
-						Таны ирээдүйн
-						өөртөө хийх
-						хөрөнгө оруулалт
-                    </h1>
-					<h5>
-						Залуу сурагчид өөрсдийн амжилтын туршлагаас хуваалцах болно.
-                    </h5>
+			<div className="text-container">
+				<div className="bold-text">
+					ТАНЫ ИРЭЭДҮЙН ӨӨРТӨӨ ХИЙХ ХӨРӨНГӨ ОРУУЛАЛТ
 				</div>
-				<h1 className="intro col l6 m6 center">
-					NOMT
-					ONLINE
-					ACADEMY
-                </h1>
+				<div className="text">
+					Таньд олон давуу тал бий болох болно
+				</div>
 			</div>
-			<Footer />
 		</div>
 	);
 };
